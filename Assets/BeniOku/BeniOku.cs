0cf49eefc4600281469241b92da0e6faf3138409
Kadir Akyol <102226254+kadirakyol@users.noreply.github.com>
2022-04-25 14:19:55 +0300

    Demo

---
+++ Assets/BeniOku/BeniOku.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeniOku : MonoBehaviour
+{
+  
+    /*
+      Bu projede yer alan asset'ler hazırdır. Lisans hakları bana ait değildir.
+      
+      Bu proje, Sn. Sinan Kaya için TEST amaçlı yapılmış olup yaklaşık 1.5 günlük süre içerisinde teslim edilmiştir.
+      
+      Bu projede hazır asset'lerin imkan verdiği kadarıyla dizayn yapılmıştır. eksikliklerin farkındayım, fakat demo
+      olduğu için üzerine düşmedim. 
+      
+      
+      Not: Assetler sadece görüntü olarak alınmıştır. Işıklandırma, yerleştirme, colliderlarını ekleme,
+      tasarımını yapma vb bütün işlemler tarafımca yapılmıştır. 
+           
+     */
+    
+    
+    
+}
